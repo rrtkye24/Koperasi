26790ae01512606ddb78cc5edb36e91804b5dfc2
rrtkye24 <retha.teja2403@gmail.com>
2022-08-05 10:15:58 +0700

    welcome

---
+++ database/migrations/2019_09_14_134705_create_anggotas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnggotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_pengguna')->unique();
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jabatan');
+            $table->string('status_menikah');
+            $table->string('nama_pasangan');
+            $table->integer('no_tlp',13);
+            $table->string('tmp_daftar');
+            $table->string('inisial')->nullable();
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggotas');
+    }
+}
